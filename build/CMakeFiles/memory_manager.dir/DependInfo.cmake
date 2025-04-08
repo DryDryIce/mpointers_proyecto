12ff3af03f129cee94c8cb4e61f6a5b623e01681
DryDryIce <167832910+DryDryIce@users.noreply.github.com>
2025-04-07 22:03:16 -0600

    build

---
+++ build/CMakeFiles/memory_manager.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alexfeng/Escritorio/Datos II/mpointers_proyecto/memory_manager/src/main.cpp" "CMakeFiles/memory_manager.dir/src/main.cpp.o" "gcc" "CMakeFiles/memory_manager.dir/src/main.cpp.o.d"
+  "/home/alexfeng/Escritorio/Datos II/mpointers_proyecto/memory_manager/src/memory_manager.grpc.pb.cc" "CMakeFiles/memory_manager.dir/src/memory_manager.grpc.pb.cc.o" "gcc" "CMakeFiles/memory_manager.dir/src/memory_manager.grpc.pb.cc.o.d"
+  "/home/alexfeng/Escritorio/Datos II/mpointers_proyecto/memory_manager/src/memory_manager.pb.cc" "CMakeFiles/memory_manager.dir/src/memory_manager.pb.cc.o" "gcc" "CMakeFiles/memory_manager.dir/src/memory_manager.pb.cc.o.d"
+  "/home/alexfeng/Escritorio/Datos II/mpointers_proyecto/memory_manager/src/memory_manager_service.cpp" "CMakeFiles/memory_manager.dir/src/memory_manager_service.cpp.o" "gcc" "CMakeFiles/memory_manager.dir/src/memory_manager_service.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/memory_manager.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/memory_manager.dir/src/main.cpp.o"
+  "CMakeFiles/memory_manager.dir/src/main.cpp.o.d"
+  "CMakeFiles/memory_manager.dir/src/memory_manager.grpc.pb.cc.o"
+  "CMakeFiles/memory_manager.dir/src/memory_manager.grpc.pb.cc.o.d"
+  "CMakeFiles/memory_manager.dir/src/memory_manager.pb.cc.o"
+  "CMakeFiles/memory_manager.dir/src/memory_manager.pb.cc.o.d"
+  "CMakeFiles/memory_manager.dir/src/memory_manager_service.cpp.o"
+  "CMakeFiles/memory_manager.dir/src/memory_manager_service.cpp.o.d"
+  "memory_manager"
+  "memory_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/memory_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
